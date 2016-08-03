6647cdbbf9fb531b387669c66be3d55e3e85b961
Raja Shaker Garlapati <rajashaker@gmail.com>
2016-08-03 13:04:04 -0700

    updated version for spring. Now test cases working fine.

--- rest-user/src/main/java/com/test/user/config/SpringDBConfig.java
+++ rest-user/src/main/java/com/test/user/config/SpringDBConfig.java
@@ -3,12 +3,11 @@ package com.test.user.config;
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
+import org.hsqldb.util.DatabaseManagerSwing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-
-import org.hsqldb.util.DatabaseManagerSwing; 
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate; 
 
 @Configuration
 public class SpringDBConfig {
